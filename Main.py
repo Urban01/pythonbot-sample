9ba0bf13800297532c26adbb745c0078cfb17e49
naoki-h1210 <naoki.h1210@gmai.com>
2018-09-29 12:35:55 +0900

    commit

--- Main.py
+++ Main.py
@@ -42,12 +42,6 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        """
-        ImageSendMessage(
-            original_content_url= 'https://loremflickr.com/480/480/' +  event.message.text,
-            preview_image_url= 'https://loremflickr.com/200/200/' +  event.message.text
-        )
-        """
         TextSendMessage(text=event.message.text)
         )
 
