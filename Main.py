e8fff74809d8ac19907355313127e04ac9f758ea
naoki-h1210 <naoki.h1210@gmai.com>
2018-09-29 12:46:17 +0900

    commit

--- Main.py
+++ Main.py
@@ -42,7 +42,10 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        ImageSendMessage(
+            original_content_url= 'https://www.google.com/search?hl=jp&q=' + event.message.text + '&btnG=Google+Search&tbs=0&safe=off&tbm=isch',
+            preview_image_url= 'https://www.google.com/search?hl=jp&q=' + event.message.text + '&btnG=Google+Search&tbs=0&safe=off&tbm=isch'
+        )
         )
 
 
